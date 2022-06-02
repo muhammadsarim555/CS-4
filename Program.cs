1ed3e7164920b0ae4cac9764ef9d7d7bf0c5ca58
Muhammad Sarim <36129452+muhammadsarim555@users.noreply.github.com>
2022-06-02 12:47:51 +0500

    Add Getter Setter work

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            human hInfo = new human();
+
+            Int32 age;
+            String name, gender;
+
+            Console.WriteLine("ENTER AGE--");
+
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("ENTER NAME--");
+
+            name = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("ENTER GENDER--");
+
+            gender = Convert.ToString(Console.ReadLine());
+
+            hInfo.setHumanInfo(age,name,gender);
+
+            Console.WriteLine(hInfo.getAge());
+            Console.WriteLine(hInfo.getName());
+            Console.WriteLine(hInfo.getGender());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ human.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class human
+    {
+        Int32 age;
+        String name, gender;
+
+        public void setHumanInfo(Int32 a, String n, String g)
+        {
+            age = a;
+            name = n;
+            gender = g;
+        }
+
+        public Int32 getAge()
+        {
+            return age;
+        }
+
+        // for getting name via getter
+        public String getName()
+        {
+            return name;
+        }
+        // for getting gender via getter
+        public String getGender()
+        {
+            return gender;
+        }
+
+    }
+}
